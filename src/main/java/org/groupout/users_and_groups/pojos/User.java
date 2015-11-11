01200ef4a81d3be1093a5de920d15fc57928f9fb
Shravan Pai <glxpai@gmail.com>
2015-11-10 23:26:25 -0800

    CODE: Committing the following changes       - adding missing dependencies to pom       - Adding HibernateUtil to access methods related to hibernate       - creating mapping and adding POJO class for User (foreign key stuff is yet to be added)       - Adding hibernate config files

---
+++ src/main/java/org/groupout/users_and_groups/pojos/User.java
@@ -0,0 +1,80 @@
+package org.groupout.users_and_groups.pojos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+	private String userId;
+	private String password;
+	private boolean active;
+	private String registeredOn;
+	private String firstName;
+	private String lastName;
+	private String emailAddress;
+	
+	public User() {
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}	
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getRegisteredOn() {
+		return registeredOn;
+	}
+
+	public void setRegisteredOn(String registeredOn) {
+		this.registeredOn = registeredOn;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+}
