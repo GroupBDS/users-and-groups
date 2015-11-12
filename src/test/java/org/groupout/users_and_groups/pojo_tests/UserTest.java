14b3451b70640c612f270d937f538704cea68b00
Shravan Pai <glxpai@gmail.com>
2015-11-12 00:58:15 -0800

    TEST: More tests for User POJO object commit 8571ccc6b8a11ec51b2e18dbfc35c1fc6cdbcf7f

--- src/test/java/org/groupout/users_and_groups/pojo_tests/UserTest.java
+++ src/test/java/org/groupout/users_and_groups/pojo_tests/UserTest.java
@@ -1,5 +1,8 @@
 package org.groupout.users_and_groups.pojo_tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Date;
 
 import org.groupout.users_and_groups.pojos.User;
@@ -9,9 +12,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class UserTest {
 	
@@ -22,7 +25,7 @@ public class UserTest {
 	private User testUserObject;
 	
 	@Before
-	public void dataSetup() {
+	public void setUpData() {
 		sessionFactory = HibernateUtil.getSessionFactory();
 		date = new Date();
 		
@@ -44,10 +47,26 @@ public class UserTest {
 		// Retrieve the saved session object
 		sessionFactory = HibernateUtil.getSessionFactory();
 		session = sessionFactory.openSession();
-		session.beginTransaction();
+		transaction = session.beginTransaction();
 		Criteria criteria = session.createCriteria(User.class);
 		criteria.add(Restrictions.eq("id", "test.user"));
 		testUserObject = (User) criteria.uniqueResult();
+		transaction.commit();
+		session.close();
+	}
+	
+	@After
+	public void clearData() {
+		sessionFactory = HibernateUtil.getSessionFactory();
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();
+		testUserObject = null;
+		testUserObject = session.load(User.class, "test.user");
+		if (testUserObject != null)
+			session.delete(testUserObject);
+		
+		transaction.commit();
+		session.close();
 	}
 	
 	@Test
@@ -60,4 +79,28 @@ public class UserTest {
 		assertEquals("Test", testUserObject.getFirstName());
 	}
 	
+	@Test
+	public void testLastName() {
+		assertEquals("User", testUserObject.getLastName());
+	}
+	
+	@Test
+	public void testEmailAddress() {
+		assertEquals("test.user@abc.com", testUserObject.getEmailAddress());
+	}
+	
+	@Test
+	public void testIsActive() {
+		assertTrue(testUserObject.isActive());
+	}
+
+	@Test
+	public void testPassword() {
+		assertEquals("xyz", testUserObject.getPassword());
+	}
+	
+	@Test
+	public void testGetRegisteredOn() {
+		assertEquals(date.toString(), testUserObject.getRegisteredOn());
+	}	
 }
